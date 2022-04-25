a2314d1571d2aecb3469ac061e50ccdb7a78bd7e
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:17:16 -0800

    Add files via upload

---
+++ Overload overloadMethods b1 ch 5 p117/Overload overloadMethods b1 ch 5 p117/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Overload//overloadMethods_b1_ch_5_p117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Overload";
+            double num;
+            double area;
+            
+            Console.Write("Please Enter Dimension In Feet: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            area = computeArea(num);
+            Console.WriteLine("\nCircle:\t\tArea = "+area+" sq.ft.");
+            area = computeArea(num, num);
+            Console.WriteLine("Square:\t\tarea = "+area+" sq.ft.");
+            area = computeArea(num, num, 'T');
+            Console.WriteLine("Triangle:\tArea = "+area+" sq.ft.");
+            Console.ReadKey();
+        }
+        static double computeArea(double width)
+        {
+            double radius = width / 2;
+            return ((radius * radius) * 3.141593);
+           
+        }
+        static double computeArea(double width, double hight)
+        {
+            
+            return (width * hight);
+          
+        }
+        static double computeArea(double width, double hight, char letter)
+        {// char never used, merely included per instruction "to determine which method should be called."
+          
+            return ((width / 2) * hight);
+        }
+    }
+}
