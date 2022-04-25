6112eb4d96945673d245822aac80b1a27c116ff2
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:58:11 -0800

    Add files via upload

---
+++ Assign  assign b1 ch3 p64/Assign  assign b1 ch3 p64/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assign //assign b1 ch3 p64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Assign";
+            int a;
+            int b;
+            Console.Write("Assign Values:");
+            Console.Write("\t a =" + (a = 8));
+            Console.WriteLine("\t b =" + (b = 4));
+            Console.Write("\n\n Add and Assign:");
+            Console.Write("\t a += b (8 += 4)\t a = " + (a += b));
+            Console.Write("\n\n Subtract and Assign:");
+            Console.Write("\t a + -= b (12 -= 4)\t a = " + (a -= b));
+            Console.Write("\n\n Multiply and Assign:");
+            Console.Write("\t a = *= b (8 *= 4)\t a =" + (a *= b));
+            Console.Write("\n\n Divide and Assign:");
+            Console.Write("\t a + /= b (32 /= 4)\t a =" + (a /= b));
+            Console.Write("\n\n Modulous and Assign:");
+            Console.Write("\t a + %= b (8 %= 4)\t a =" + (a %= b));
+            Console.ReadKey();
+        }
+    }
+}
