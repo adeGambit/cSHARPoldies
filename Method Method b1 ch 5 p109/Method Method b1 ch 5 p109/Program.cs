e7f2324366fbc19eb13e34e369b19bdfbe45a64b
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:51:29 -0800

    Add files via upload

---
+++ Method Method b1 ch 5 p109/Method Method b1 ch 5 p109/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Method//Method_b1_ch_5_p109
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Method";
+            bodyTempC();
+            Console.WriteLine("Farenheit:\t" + bodyTempF() + "*F");
+            Console.WriteLine("Kelvin:\t" + bodyTempK() + "K");
+           Console.ReadKey();
+            
+        }
+        static void bodyTempC()
+        {
+            Console.WriteLine("Body Temperature...");
+            Console.WriteLine("Centigrade:\t37*C");
+        }
+        static double bodyTempF()
+        {
+            double temperature = 98.6;
+            return temperature;
+        }
+        static int bodyTempK()
+        {
+            int temperature = 310;
+            return temperature;
+        }
+    }
+}
