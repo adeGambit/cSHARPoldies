ced2a2ed93261086e583125dc0159734575cad1e
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:23:58 -0800

    Add files via upload

---
+++ Switching Branches b1 ch 4 p90/Switching Branches b1 ch 4 p90/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Switching_Branches_b1_ch_4_p90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Switch";
+            int num = 3;
+            string day;
+            switch (num)
+            {
+                case 1: day = "Monday";break;
+                case 2: day = "Tuesday";break;
+                case 3: day = "Wednesday";break;
+                case 4: day = "Thursday";break;
+                case 5: day = "Friday";break;
+                //Default statement to be inserted here (Step5)
+                default: day = "weekend Day";break;
+            }
+            Console.WriteLine("Day" + num + ":" + day);
+            Console.ReadKey();
+        }
+    }
+}
