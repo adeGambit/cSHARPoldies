c456b20614e6239b17dc61f44ab52b36e2e1940d
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:02:13 -0800

    Add files via upload

---
+++ Constant Constant b1 ch2 p53/Constant Constant b1 ch2 p53/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constant//Constant_b1_ch2_p53
+{
+    class Program
+    {enum Days { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+        static void Main(string[] args)
+        {
+            Console.Title = "Constant";
+            const double pi = 3.14159265358979;
+            var daysType = typeof( Days );
+            Console.WriteLine("Pi Type: " + pi.GetType());
+            Console.WriteLine("Circumference: " + (pi * 3));
+            Console.WriteLine("\nFirst Name: " + Days.Sat);
+            Console.WriteLine("1st Index: " + (int) Days.Sat);
+            string name = Enum.GetName(daysType, 1);
+            Console.WriteLine("\n2ndIndex: " + name );
+            bool flag = Enum.IsDefined(daysType, "Mon");
+            Console.WriteLine("Contains Mon? "+ flag);
+            Console.ReadKey();
+        }
+    }
+}
