38c33caae0b83d41ebe85a57ef6496a1149cbce9
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:25:41 -0800

    Add files via upload

---
+++ While Loop whileLoop b1 ch 4 p98/While Loop whileLoop b1 ch 4 p98/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While_Loop//whileLoop_b1_ch_4_p98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            int i = 0;
+            Console.Title = "While Loop";
+            while (1<nums.Length)
+            {
+                nums[1] = 1;
+                Console.Write("|" + nums[i]);
+                i++;
+            }
+            Console.Write("\n\n");
+            do
+            {
+                i--;
+                //Statements to be inserted here (Step6)
+            } 
+            while (i>0);
+            Console.ReadKey();
+        }
+    }
+}
