afe55719203e75bb76c75f9ff0686ef7ef5c5ce0
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:58:47 -0800

    Add files via upload

---
+++ BookIIsetupANDstart B2p27PDFP44SECTION1.8/BookIIsetupANDstart B2p27PDFP44SECTION1.8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BookIIsetupANDstart_B2p27PDFP44SECTION1._8//CHAPTER2 example tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Title = "Form1";
+            Console.ReadKey();
+        }
+    }
+}
