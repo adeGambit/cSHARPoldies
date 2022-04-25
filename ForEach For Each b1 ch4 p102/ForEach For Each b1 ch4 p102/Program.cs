c43c00854c6ab4f2eca59d013bdf3ec6c56aada4
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:05:55 -0800

    Add files via upload

---
+++ ForEach For Each b1 ch4 p102/ForEach For Each b1 ch4 p102/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForEach//For_Each_b1_ch4_p102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ForEach";
+            string[] websites = new string[5] { "Google", "YouTube", "Facebook", "Baidu", "Yahoo!" };
+            Console.WriteLine("Popular Websites...");
+            int rank = 1;
+            foreach (string site in websites)
+            {
+                Console.WriteLine("Position:" + rank + "\t" + site);
+                rank++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
