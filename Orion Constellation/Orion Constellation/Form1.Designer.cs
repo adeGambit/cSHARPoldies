34e58680955442db5bca6ac7c2a569b7cbdfd8bc
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:16:36 -0800

    Add files via upload

---
+++ Orion Constellation/Orion Constellation/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Orion_Constellation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.betelgeuseLabel = new System.Windows.Forms.Label();
+            this.meissaLabel = new System.Windows.Forms.Label();
+            this.mintakaLabel = new System.Windows.Forms.Label();
+            this.alnilamButton = new System.Windows.Forms.Label();
+            this.alnitakLabel = new System.Windows.Forms.Label();
+            this.saiphLabel = new System.Windows.Forms.Label();
+            this.rigelLabel = new System.Windows.Forms.Label();
+            this.ic434Label = new System.Windows.Forms.Label();
+            this.m42Label = new System.Windows.Forms.Label();
+            this.m43Label = new System.Windows.Forms.Label();
+            this.showStarNamesButton = new System.Windows.Forms.Button();
+            this.hideStarNamesButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Orion_Constellation.Properties.Resources.orion;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(615, 683);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // betelgeuseLabel
+            // 
+            this.betelgeuseLabel.AutoSize = true;
+            this.betelgeuseLabel.Location = new System.Drawing.Point(58, 91);
+            this.betelgeuseLabel.Name = "betelgeuseLabel";
+            this.betelgeuseLabel.Size = new System.Drawing.Size(90, 20);
+            this.betelgeuseLabel.TabIndex = 1;
+            this.betelgeuseLabel.Text = "Betelgeuse";
+            this.betelgeuseLabel.Visible = false;
+            // 
+            // meissaLabel
+            // 
+            this.meissaLabel.AutoSize = true;
+            this.meissaLabel.Location = new System.Drawing.Point(343, 144);
+            this.meissaLabel.Name = "meissaLabel";
+            this.meissaLabel.Size = new System.Drawing.Size(59, 20);
+            this.meissaLabel.TabIndex = 2;
+            this.meissaLabel.Text = "Meissa";
+            this.meissaLabel.Visible = false;
+            // 
+            // mintakaLabel
+            // 
+            this.mintakaLabel.AutoSize = true;
+            this.mintakaLabel.Location = new System.Drawing.Point(381, 331);
+            this.mintakaLabel.Name = "mintakaLabel";
+            this.mintakaLabel.Size = new System.Drawing.Size(65, 20);
+            this.mintakaLabel.TabIndex = 3;
+            this.mintakaLabel.Text = "Mintaka";
+            this.mintakaLabel.Visible = false;
+            // 
+            // alnilamButton
+            // 
+            this.alnilamButton.AutoSize = true;
+            this.alnilamButton.Location = new System.Drawing.Point(247, 288);
+            this.alnilamButton.Name = "alnilamButton";
+            this.alnilamButton.Size = new System.Drawing.Size(60, 20);
+            this.alnilamButton.TabIndex = 4;
+            this.alnilamButton.Text = "Alnilam";
+            this.alnilamButton.Visible = false;
+            // 
+            // alnitakLabel
+            // 
+            this.alnitakLabel.AutoSize = true;
+            this.alnitakLabel.Location = new System.Drawing.Point(137, 367);
+            this.alnitakLabel.Name = "alnitakLabel";
+            this.alnitakLabel.Size = new System.Drawing.Size(57, 20);
+            this.alnitakLabel.TabIndex = 5;
+            this.alnitakLabel.Text = "Alnitak";
+            this.alnitakLabel.Visible = false;
+            // 
+            // saiphLabel
+            // 
+            this.saiphLabel.AutoSize = true;
+            this.saiphLabel.Location = new System.Drawing.Point(182, 558);
+            this.saiphLabel.Name = "saiphLabel";
+            this.saiphLabel.Size = new System.Drawing.Size(50, 20);
+            this.saiphLabel.TabIndex = 6;
+            this.saiphLabel.Text = "Saiph";
+            this.saiphLabel.Visible = false;
+            // 
+            // rigelLabel
+            // 
+            this.rigelLabel.AutoSize = true;
+            this.rigelLabel.Location = new System.Drawing.Point(418, 558);
+            this.rigelLabel.Name = "rigelLabel";
+            this.rigelLabel.Size = new System.Drawing.Size(45, 20);
+            this.rigelLabel.TabIndex = 7;
+            this.rigelLabel.Text = "Rigel";
+            this.rigelLabel.Visible = false;
+            // 
+            // ic434Label
+            // 
+            this.ic434Label.AutoSize = true;
+            this.ic434Label.Location = new System.Drawing.Point(230, 427);
+            this.ic434Label.Name = "ic434Label";
+            this.ic434Label.Size = new System.Drawing.Size(52, 20);
+            this.ic434Label.TabIndex = 8;
+            this.ic434Label.Text = "IC434";
+            this.ic434Label.Visible = false;
+            // 
+            // m42Label
+            // 
+            this.m42Label.AutoSize = true;
+            this.m42Label.Location = new System.Drawing.Point(230, 471);
+            this.m42Label.Name = "m42Label";
+            this.m42Label.Size = new System.Drawing.Size(40, 20);
+            this.m42Label.TabIndex = 9;
+            this.m42Label.Text = "M42";
+            this.m42Label.Visible = false;
+            // 
+            // m43Label
+            // 
+            this.m43Label.AutoSize = true;
+            this.m43Label.Location = new System.Drawing.Point(230, 506);
+            this.m43Label.Name = "m43Label";
+            this.m43Label.Size = new System.Drawing.Size(40, 20);
+            this.m43Label.TabIndex = 10;
+            this.m43Label.Text = "M43";
+            this.m43Label.Visible = false;
+            // 
+            // showStarNamesButton
+            // 
+            this.showStarNamesButton.Location = new System.Drawing.Point(34, 612);
+            this.showStarNamesButton.Name = "showStarNamesButton";
+            this.showStarNamesButton.Size = new System.Drawing.Size(198, 48);
+            this.showStarNamesButton.TabIndex = 11;
+            this.showStarNamesButton.Text = "Show Star Names";
+            this.showStarNamesButton.UseVisualStyleBackColor = true;
+            this.showStarNamesButton.Click += new System.EventHandler(this.showStarNamesButton_Click);
+            // 
+            // hideStarNamesButton
+            // 
+            this.hideStarNamesButton.Location = new System.Drawing.Point(394, 612);
+            this.hideStarNamesButton.Name = "hideStarNamesButton";
+            this.hideStarNamesButton.Size = new System.Drawing.Size(190, 48);
+            this.hideStarNamesButton.TabIndex = 12;
+            this.hideStarNamesButton.Text = "Hide Star Names";
+            this.hideStarNamesButton.UseVisualStyleBackColor = true;
+            this.hideStarNamesButton.Click += new System.EventHandler(this.hideStarNamesButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(251, 612);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(123, 42);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 672);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.hideStarNamesButton);
+            this.Controls.Add(this.showStarNamesButton);
+            this.Controls.Add(this.m43Label);
+            this.Controls.Add(this.m42Label);
+            this.Controls.Add(this.ic434Label);
+            this.Controls.Add(this.rigelLabel);
+            this.Controls.Add(this.saiphLabel);
+            this.Controls.Add(this.alnitakLabel);
+            this.Controls.Add(this.alnilamButton);
+            this.Controls.Add(this.mintakaLabel);
+            this.Controls.Add(this.meissaLabel);
+            this.Controls.Add(this.betelgeuseLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "The Migty Orion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label betelgeuseLabel;
+        private System.Windows.Forms.Label meissaLabel;
+        private System.Windows.Forms.Label mintakaLabel;
+        private System.Windows.Forms.Label alnilamButton;
+        private System.Windows.Forms.Label alnitakLabel;
+        private System.Windows.Forms.Label saiphLabel;
+        private System.Windows.Forms.Label rigelLabel;
+        private System.Windows.Forms.Label ic434Label;
+        private System.Windows.Forms.Label m42Label;
+        private System.Windows.Forms.Label m43Label;
+        private System.Windows.Forms.Button showStarNamesButton;
+        private System.Windows.Forms.Button hideStarNamesButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Orion Constellation/Orion Constellation/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orion_Constellation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showStarNamesButton_Click(object sender, EventArgs e)
+        {
+            betelgeuseLabel.Visible = true;
+            meissaLabel.Visible = true;
+            mintakaLabel.Visible = true;
+            alnilamButton.Visible = true;
+            alnitakLabel.Visible = true;
+            saiphLabel.Visible = true;
+            rigelLabel.Visible = true;
+            //extra credit via representations lack of bare minimum completion
+            ic434Label.Visible = true;
+            m42Label.Visible = true;
+            m43Label.Visible = true;
+        }
+
+        private void hideStarNamesButton_Click(object sender, EventArgs e)
+        {
+            betelgeuseLabel.Visible = false;
+            meissaLabel.Visible = false;
+            mintakaLabel.Visible = false;
+            alnilamButton.Visible = false;
+            alnitakLabel.Visible = false;
+            saiphLabel.Visible = false;
+            rigelLabel.Visible = false;
+
+            ic434Label.Visible = false;
+            m42Label.Visible = false;
+            m43Label.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //no click required, this was added so the picure would show up at all 
+            //shortly later, i fugured out that the picture's visibility property
+            //was set to false for some odd reason and i left this section to 
+            //remind myself and answer any questions about what mistakes i discovered
+            //myself making, in addition to typing this out and LINE commenting in
+            //stread of BLOCK commenting. 2 mistakes for the price of one.
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
