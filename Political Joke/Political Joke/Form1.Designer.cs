e17ef7995b115ee1d7ca5f559ba437d31bb31e47
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:19:01 -0800

    Add files via upload

---
+++ Political Joke/Political Joke/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Political_Joke
+{
+    partial class poloticalJoke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jokeLabel = new System.Windows.Forms.Label();
+            this.leftWingButton = new System.Windows.Forms.Button();
+            this.rightWingButton = new System.Windows.Forms.Button();
+            this.setUpLabel = new System.Windows.Forms.Label();
+            this.leftistPunchLineDislpay = new System.Windows.Forms.Label();
+            this.rightistSetUpDislpay = new System.Windows.Forms.Label();
+            this.leftPunchLineButton = new System.Windows.Forms.Button();
+            this.rightistPunchLineButton = new System.Windows.Forms.Button();
+            this.punchLineLabel = new System.Windows.Forms.Label();
+            this.leftistPunchLineLabel = new System.Windows.Forms.Label();
+            this.rightistPunchLineLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // jokeLabel
+            // 
+            this.jokeLabel.AutoSize = true;
+            this.jokeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jokeLabel.Location = new System.Drawing.Point(277, 27);
+            this.jokeLabel.Name = "jokeLabel";
+            this.jokeLabel.Size = new System.Drawing.Size(297, 32);
+            this.jokeLabel.TabIndex = 0;
+            this.jokeLabel.Text = "What happens when...";
+            // 
+            // leftWingButton
+            // 
+            this.leftWingButton.Location = new System.Drawing.Point(58, 134);
+            this.leftWingButton.Name = "leftWingButton";
+            this.leftWingButton.Size = new System.Drawing.Size(163, 38);
+            this.leftWingButton.TabIndex = 1;
+            this.leftWingButton.Text = "Left Wing Wins?";
+            this.leftWingButton.UseVisualStyleBackColor = true;
+            this.leftWingButton.Click += new System.EventHandler(this.leftWingButton_Click);
+            // 
+            // rightWingButton
+            // 
+            this.rightWingButton.Location = new System.Drawing.Point(587, 134);
+            this.rightWingButton.Name = "rightWingButton";
+            this.rightWingButton.Size = new System.Drawing.Size(162, 38);
+            this.rightWingButton.TabIndex = 2;
+            this.rightWingButton.Text = "Right Wing Wins?";
+            this.rightWingButton.UseVisualStyleBackColor = true;
+            this.rightWingButton.Click += new System.EventHandler(this.rightWingButton_Click);
+            // 
+            // setUpLabel
+            // 
+            this.setUpLabel.AutoSize = true;
+            this.setUpLabel.Location = new System.Drawing.Point(343, 134);
+            this.setUpLabel.Name = "setUpLabel";
+            this.setUpLabel.Size = new System.Drawing.Size(127, 20);
+            this.setUpLabel.TabIndex = 3;
+            this.setUpLabel.Text = "<choose set up>";
+            // 
+            // leftistPunchLineDislpay
+            // 
+            this.leftistPunchLineDislpay.AutoSize = true;
+            this.leftistPunchLineDislpay.Location = new System.Drawing.Point(129, 211);
+            this.leftistPunchLineDislpay.Name = "leftistPunchLineDislpay";
+            this.leftistPunchLineDislpay.Size = new System.Drawing.Size(0, 20);
+            this.leftistPunchLineDislpay.TabIndex = 4;
+            // 
+            // rightistSetUpDislpay
+            // 
+            this.rightistSetUpDislpay.AutoSize = true;
+            this.rightistSetUpDislpay.Location = new System.Drawing.Point(637, 196);
+            this.rightistSetUpDislpay.Name = "rightistSetUpDislpay";
+            this.rightistSetUpDislpay.Size = new System.Drawing.Size(0, 20);
+            this.rightistSetUpDislpay.TabIndex = 5;
+            // 
+            // leftPunchLineButton
+            // 
+            this.leftPunchLineButton.Location = new System.Drawing.Point(68, 275);
+            this.leftPunchLineButton.Name = "leftPunchLineButton";
+            this.leftPunchLineButton.Size = new System.Drawing.Size(154, 36);
+            this.leftPunchLineButton.TabIndex = 6;
+            this.leftPunchLineButton.Text = "Left Punch Line";
+            this.leftPunchLineButton.UseVisualStyleBackColor = true;
+            this.leftPunchLineButton.Click += new System.EventHandler(this.leftPunchLineButton_Click);
+            // 
+            // rightistPunchLineButton
+            // 
+            this.rightistPunchLineButton.Location = new System.Drawing.Point(587, 264);
+            this.rightistPunchLineButton.Name = "rightistPunchLineButton";
+            this.rightistPunchLineButton.Size = new System.Drawing.Size(162, 41);
+            this.rightistPunchLineButton.TabIndex = 7;
+            this.rightistPunchLineButton.Text = "Right Punch Line";
+            this.rightistPunchLineButton.UseVisualStyleBackColor = true;
+            this.rightistPunchLineButton.Click += new System.EventHandler(this.rightistPunchLineButton_Click);
+            // 
+            // punchLineLabel
+            // 
+            this.punchLineLabel.AutoSize = true;
+            this.punchLineLabel.Location = new System.Drawing.Point(330, 274);
+            this.punchLineLabel.Name = "punchLineLabel";
+            this.punchLineLabel.Size = new System.Drawing.Size(155, 20);
+            this.punchLineLabel.TabIndex = 8;
+            this.punchLineLabel.Text = "<choose punch line>";
+            // 
+            // leftistPunchLineLabel
+            // 
+            this.leftistPunchLineLabel.AutoSize = true;
+            this.leftistPunchLineLabel.Location = new System.Drawing.Point(92, 340);
+            this.leftistPunchLineLabel.Name = "leftistPunchLineLabel";
+            this.leftistPunchLineLabel.Size = new System.Drawing.Size(0, 20);
+            this.leftistPunchLineLabel.TabIndex = 9;
+            // 
+            // rightistPunchLineLabel
+            // 
+            this.rightistPunchLineLabel.AutoSize = true;
+            this.rightistPunchLineLabel.Location = new System.Drawing.Point(623, 340);
+            this.rightistPunchLineLabel.Name = "rightistPunchLineLabel";
+            this.rightistPunchLineLabel.Size = new System.Drawing.Size(0, 20);
+            this.rightistPunchLineLabel.TabIndex = 10;
+            // 
+            // poloticalJoke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rightistPunchLineLabel);
+            this.Controls.Add(this.leftistPunchLineLabel);
+            this.Controls.Add(this.punchLineLabel);
+            this.Controls.Add(this.rightistPunchLineButton);
+            this.Controls.Add(this.leftPunchLineButton);
+            this.Controls.Add(this.rightistSetUpDislpay);
+            this.Controls.Add(this.leftistPunchLineDislpay);
+            this.Controls.Add(this.setUpLabel);
+            this.Controls.Add(this.rightWingButton);
+            this.Controls.Add(this.leftWingButton);
+            this.Controls.Add(this.jokeLabel);
+            this.Name = "poloticalJoke";
+            this.Text = "Obligatory Political Joke";
+            this.Load += new System.EventHandler(this.poloticalJoke_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label jokeLabel;
+        private System.Windows.Forms.Button leftWingButton;
+        private System.Windows.Forms.Button rightWingButton;
+        private System.Windows.Forms.Label setUpLabel;
+        private System.Windows.Forms.Label leftistPunchLineDislpay;
+        private System.Windows.Forms.Label rightistSetUpDislpay;
+        private System.Windows.Forms.Button leftPunchLineButton;
+        private System.Windows.Forms.Button rightistPunchLineButton;
+        private System.Windows.Forms.Label punchLineLabel;
+        private System.Windows.Forms.Label leftistPunchLineLabel;
+        private System.Windows.Forms.Label rightistPunchLineLabel;
+    }
+}
+
---
+++ Political Joke/Political Joke/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Political_Joke
+{
+    public partial class poloticalJoke : Form
+    {
+        public poloticalJoke()
+        {
+            InitializeComponent();
+        }
+
+        private void leftWingButton_Click(object sender, EventArgs e)
+        {
+            leftistPunchLineDislpay.Text = "Look Right";
+        }
+
+        private void rightWingButton_Click(object sender, EventArgs e)
+        {
+            rightistSetUpDislpay.Text = "Look Left";
+        }
+
+        private void leftPunchLineButton_Click(object sender, EventArgs e)
+        {
+            leftistPunchLineDislpay.Text = "nothing is left";
+        }
+
+        private void rightistPunchLineButton_Click(object sender, EventArgs e)
+        {
+            rightistSetUpDislpay.Text = "nothing is right";
+        }
+        private void poloticalJoke_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
