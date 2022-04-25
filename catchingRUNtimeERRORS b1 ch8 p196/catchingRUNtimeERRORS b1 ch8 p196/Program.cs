37f449e421b3252c3d2cc1628de769d8dcd164c1
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:59:57 -0800

    Add files via upload

---
+++ catchingRUNtimeERRORS b1 ch8 p196/catchingRUNtimeERRORS b1 ch8 p196/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace catchingRUNtimeERRORS_b1_ch8_p196
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cathing Run-Time Errors";
+
+            try
+            {
+                Console.WriteLine("Please Enter A Number: ");
+                double num1 = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Now Enter Another Number: ");
+                double num2 = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Total: " + (num1 + num2));
+            }
+            catch (OverflowException)
+            { Console.WriteLine("\nMaximum: "+ Int16.MaxValue); }
+        }
+    }
+}
