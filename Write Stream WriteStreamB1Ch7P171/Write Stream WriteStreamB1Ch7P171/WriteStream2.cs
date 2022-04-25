a5e2f032c3cfbc84bcf33cb119b8c345723d4acb
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:27:58 -0800

    Add files via upload

---
+++ Write Stream WriteStreamB1Ch7P171/Write Stream WriteStreamB1Ch7P171/WriteStream2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Write_Stream//WriteStreamB1Ch7P171
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "WriteStream";
+
+            string path = "C:\\User\\lower\\OneDrive\\Desktop\\robert.txt";
+            string[] poem = new string[]
+                { 
+                 "\tThis truth finds honest Tam o'Shanter",
+                "\n\tAs he from Ayr one night did canter",
+                "\n\tOld Ayr, which never a town surpasse",
+                "\n\tFor honest men and bonnie lasses."
+                };
+
+            string attrib = "\r\n\tTam O'Shanter (Robert Burns 1790)";
+
+            try
+            {
+                //statments to be inserted (steps 5&6)
+                using (StreamWriter writer = new StreamWriter(path))//5
+                {
+                    foreach (string line in poem)
+                    { writer.WriteLine(line); }
+
+                }
+                using (StreamWriter writer = new StreamWriter(path, true))//6
+                {
+                    writer.WriteLine(attrib);
+                    writer.WriteLine("File Written: " + path);
+                }
+
+            }
+            catch (Exception error)
+            { Console.WriteLine(error.Message); }
+            Console.ReadKey();
+        }
+    }
+}
