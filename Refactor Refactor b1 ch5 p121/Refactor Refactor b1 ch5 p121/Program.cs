30539eba27a2121ccd20995048ef7bb5cbbd8c37
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:22:13 -0800

    Add files via upload

---
+++ Refactor Refactor b1 ch5 p121/Refactor Refactor b1 ch5 p121/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Refactor_Refactor_b1_ch5_p121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Refactor";
+            computeFactorials(1, 8);
+            Console.ReadKey();
+        }
+        static int factorial(int num)
+        {
+            int result;
+            if(num == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = (factorial(num - 1) * num);
+            }
+            return result;
+        }
+        static void computeFactorials(int num, int max)
+        {
+            while (num <= max)
+            {
+                Console.Write("Factorial Of " + num + " : ");
+                Console.WriteLine(factorial(num));
+                num++;
+            }
+        }
+
+    }
+}
