639a8107bc1efbf5379cf783671c201f61230cde
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:56:28 -0800

    Add files via upload

---
+++ Arrays b1 ch2 p46/Arrays b1 ch2 p46/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrays//Arrays_b1_ch2_p46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arrays";
+            string[] cars = new string[3] { "BMW", "Ford", "Opel" };
+            int[,] coords = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            Console.WriteLine("Second Car: " + cars[1]);
+            Console.WriteLine("X1,Y1 " + coords[0,0]);
+            Console.WriteLine("X2,Y3 " + coords[1,2]);
+            Console.ReadKey();
+        }
+    }
+}
