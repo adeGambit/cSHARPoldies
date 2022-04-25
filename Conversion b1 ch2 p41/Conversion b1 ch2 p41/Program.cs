553a65f6fd215c43df63bab82e9f260b1a21f476
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:02:43 -0800

    Add files via upload

---
+++ Conversion b1 ch2 p41/Conversion b1 ch2 p41/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Conversion//Conversion_b1_ch2_p41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Conversion";
+            Console.WriteLine("Please Enter A Number:");
+            Double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now Enter Another Number:");
+            double sum = num + Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Total = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
