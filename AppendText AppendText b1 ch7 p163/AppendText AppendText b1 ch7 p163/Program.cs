2211fed42e647ff32fcd7e3a763487504771c002
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:52:41 -0800

    Add files via upload

---
+++ AppendText AppendText b1 ch7 p163/AppendText AppendText b1 ch7 p163/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+namespace AppendText//AppendText_b1_ch7_p163
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="AppendText";
+
+            string path = "C:\\Users\\lower\\OneDrive\\Desktop\\oscar.txt";
+            string[] poem = new string[]
+            {
+                "\t\n Debtors' Yard the stones are hard",
+                "\tAnd the dripping wall is high"
+            };
+            string attrib = "\r\n\tThe Ballad of Reading Gaol";
+            attrib += "(Oscar Wilde 1898)";
+            if(File.Exists(path))
+            {
+                //Statements to be inserted here ( step 5 ).
+                File.AppendAllText(path, attrib);
+                Console.WriteLine("Appended to File: " + path);
+            }
+            else
+            {
+                //Statements to be inserted here (step6).
+                try
+                {
+                    File.WriteAllLines(path, poem);
+                    Console.WriteLine("File Written: " + path);
+                }
+                catch(Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
