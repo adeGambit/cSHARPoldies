00f05fda163bb9e79b4933a3cb53978eb9b9cf8a
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:49:54 -0800

    Add files via upload

---
+++ Manipulate Manipulate b1 ch6 p132/Manipulate Manipulate b1 ch6 p132/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Manipulate//Manipulate_b1_ch6_p132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Manipulate";
+            Console.WriteLine("Please Enter Text: ");
+            string text = Console.ReadLine();
+            Console.Write("\nThanks! You Entered: \n'"+ text + "'");
+            Console.WriteLine("\t\tText Length:'"+ text.Length + "'");
+            text = text.Trim();
+            Console.Write("\nTrimmed:'"+text+"'");
+            string upper = text.ToUpper();
+            Console.WriteLine("\nUpperCase:\t" + upper + "'");
+            string lower = text.ToLower(); 
+            Console.WriteLine("LowerCase:\t'"+lower+"'");
+            upper = upper.PadLeft(40);
+            lower = lower.PadRight(40,'#');
+            text = text.PadLeft(30, '*').PadRight(40,'*');
+            Console.WriteLine("\nPadded Left: \t'"+upper+"'");
+            Console.WriteLine("Padded Right: \t'"+lower+"'");
+            Console.WriteLine("Padded Both: \t'"+text+"'");
+            Console.WriteLine("\nTrimmed Start:\t'"+upper.TrimStart()+"'");
+            Console.WriteLine("Trimmed End: \t'"+text.TrimEnd('*')+"'");
+            Console.ReadKey();
+        }
+    }
+}
