23a505fe754c565d0224db48c00641be40cd6458
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:19:51 -0800

    Add files via upload

---
+++ Precedence Precedence b1 ch3 p80/Precedence Precedence b1 ch3 p80/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Precedence//Precedence_b1_ch3_p80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Precidence";
+            int sum;
+            sum = 1 + 4 * 3;
+            Console.Write("Default Order:\t\t" + sum);
+            sum = (1 + 4) * 3;
+            Console.Write("\nForced Order:\t\t" + sum + "\n");
+            sum = 7 - 4 + 2;
+            Console.Write("\nDefault Directions:\t" + sum);
+            sum = 7 - (4 + 2);
+            Console.Write("\nForced Direction:\t" + sum);
+            Console.ReadKey();
+            //2new line additions to match example format
+        }
+    }
+}
