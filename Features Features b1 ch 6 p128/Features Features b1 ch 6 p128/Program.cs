ecd67354c52b53fb4e1414edfe2b5e3249575edb
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:05:23 -0800

    Add files via upload

---
+++ Features Features b1 ch 6 p128/Features Features b1 ch 6 p128/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Features_Features_b1_ch_6_p128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Features";
+            Console.Write("Please Enter Text: ");
+            string text = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine("\nERROR: No Text Found!");
+            }
+            else
+            {
+                //Statements to be inserted (for Steps4-7).
+                Console.WriteLine("\nThanks. You Entered:\n'"+text+"'");
+                Console.WriteLine("\nText Lenght: "+text.Length);
+                string query = text.StartsWith("C#") ? "Does " : "Does Not ";
+                Console.WriteLine("Text " + query + "Start with'C#'");
+                query = text.EndsWith("steps") ? "Does " : "Does Not ";
+                Console.WriteLine("Text 5"+query+"Contain 'easy'");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
