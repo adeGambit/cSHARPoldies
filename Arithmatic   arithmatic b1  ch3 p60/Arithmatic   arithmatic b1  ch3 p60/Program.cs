7103bf9a729d9c8ba393b39b4663a01d8ea2800d
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 21:55:23 -0800

    Add files via upload

---
+++ Arithmatic   arithmatic b1  ch3 p60/Arithmatic   arithmatic b1  ch3 p60/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arithmatic//arithmatic_b1__ch3_p60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arithmatic";
+            int a = 8;
+            int b = 4;
+            Console.WriteLine("Addition:\t" + (a + b));
+            Console.WriteLine("Subtract:\t" + ( a - b));
+            Console.WriteLine("Multiplication:\t" + (a * b));
+            Console.WriteLine("Divisiont:\t" + (a/b));
+            Console.WriteLine("Modulous:\t" + (a%b));
+            Console.WriteLine("\nPostFix:\t" + (a++));
+            Console.WriteLine("\nPostFIx.....:\t" + a);
+            Console.WriteLine("\nPostFix:\t" + (b++));
+            Console.WriteLine("\nPostFIx.....:\t" + b);
+        }
+    }
+}
