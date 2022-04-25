b24362a1f061df5f8e9fc06ae474ad356c6ae0b9
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:10:31 -0800

    Add files via upload
    limited language translator

---
+++ Language Translator/Language Translator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Language_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.translationLabel = new System.Windows.Forms.Label();
+            this.italianButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.spanishButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Language and I will say Good Morning.";
+            this.label1.Click += new System.EventHandler(this.instructionLabel_Click);
+            // 
+            // translationLabel
+            // 
+            this.translationLabel.AutoSize = true;
+            this.translationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.translationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translationLabel.Location = new System.Drawing.Point(333, 110);
+            this.translationLabel.MaximumSize = new System.Drawing.Size(3, 1);
+            this.translationLabel.Name = "translationLabel";
+            this.translationLabel.Size = new System.Drawing.Size(2, 1);
+            this.translationLabel.TabIndex = 1;
+            this.translationLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.translationLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // italianButton
+            // 
+            this.italianButton.AccessibleName = "";
+            this.italianButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.italianButton.Location = new System.Drawing.Point(101, 239);
+            this.italianButton.Name = "italianButton";
+            this.italianButton.Size = new System.Drawing.Size(75, 29);
+            this.italianButton.TabIndex = 2;
+            this.italianButton.Text = "Italian";
+            this.italianButton.UseVisualStyleBackColor = true;
+            this.italianButton.Click += new System.EventHandler(this.italianButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button3.Location = new System.Drawing.Point(560, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "German";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.germanButton_Click);
+            // 
+            // spanishButton
+            // 
+            this.spanishButton.AccessibleName = "";
+            this.spanishButton.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Language_Translator.Properties.Settings.Default, "spanishButton", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.spanishButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.spanishButton.Location = new System.Drawing.Point(333, 239);
+            this.spanishButton.Name = "spanishButton";
+            this.spanishButton.Size = new System.Drawing.Size(96, 29);
+            this.spanishButton.TabIndex = 3;
+            this.spanishButton.Text = global::Language_Translator.Properties.Settings.Default.spanishButton;
+            this.spanishButton.UseVisualStyleBackColor = true;
+            this.spanishButton.Click += new System.EventHandler(this.spanishButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.spanishButton);
+            this.Controls.Add(this.italianButton);
+            this.Controls.Add(this.translationLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Language Translator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label translationLabel;
+        private System.Windows.Forms.Button italianButton;
+        private System.Windows.Forms.Button spanishButton;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
