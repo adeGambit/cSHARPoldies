3cb87ae1873acb707527f4e1230a572eb24ee0e2
ADE <97244700+adeGambit@users.noreply.github.com>
2022-04-24 22:06:32 -0800

    Add files via upload

---
+++ ForLoop For Loop b1 ch4 p95/ForLoop For Loop b1 ch4 p95/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoop//For_Loop_b1_ch4_p95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ForLoop";
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("Loop Iteration: " + i);
+                //Nested Loop to be inserted here ( step 4)
+                for (int j = 1; j < 4; j++)
+                {
+                    Console.WriteLine("\tInner Loop Iteration: " + j);
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
